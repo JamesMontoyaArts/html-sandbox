9bdb091f87c869a9b2cd52d4e96fa00966b66103
James Montoya <jamesmontoyaarts@gmail.com>
2016-10-11 13:38:08 -0600

    added section 1

--- public_html/index.php
+++ public_html/index.php
@@ -5,6 +5,22 @@
 		<title>HTML Sandbox</title>
 	</head>
 	<body>
-		<h1>Hello World!</h1>
+
+		<!--This is the header -->
+		<header>
+			<h1>Hi! This is my page.</h1>
+		</header>
+
+		<!-- This is the main page content-->
+		<main>
+
+			<!-- begin section 1-->
+			<section>
+				<p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends.</p>
+					<img src="img/Art%20fight%207.jpg" alt="art fight 7">
+			</section>
+			<section></section>
+			<section></section>
+		</main>
 	</body>
 </html>
