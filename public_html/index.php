fc17c7a552e4637932b4fccd9b1926c4df4b5e6c
James Montoya <jamesmontoyaarts@gmail.com>
2016-10-11 15:15:00 -0600

    created style sheet

--- public_html/index.php
+++ public_html/index.php
@@ -2,6 +2,10 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
+
+		<!--Add CSS file-->
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css" />
+
 		<title>HTML Sandbox</title>
 	</head>
 	<body>
@@ -19,8 +23,44 @@
 				<p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends.</p>
 					<img src="img/Art%20fight%207.jpg" alt="art fight 7">
 			</section>
-			<section></section>
-			<section></section>
+
+			<!--begin section 2-->
+			<section>
+				<h2>Hip Hop N' Hops</h2>
+				<ul>
+					<li>James Montoya</li>
+					<li>Monty Singer</li>
+					<li>Chis Lopez</li>
+					<li>Adriana Lopez</li>
+				</ul>
+			</section>
+
+			<!--begin section 3-->
+			<section>
+				<table>
+					<tr>
+						<th>Artist</th>
+						<th>Medium</th>
+					</tr>
+
+					<tr>
+						<td>James</td>
+						<td>Paint</td>
+					</tr>
+
+					<tr>
+						<td>Monty</td>
+						<td>Pastels</td>
+					</tr>
+
+					<tr>
+						<td>Adriana</td>
+						<td>Paint</td>
+					</tr>
+
+				</table>
+			</section>
+
 		</main>
 	</body>
 </html>
